6fe228d7ce186a31642bbf8eca29721f2a92bd37
serigne-saliou sow <saliousow0208@gmail.com>
2024-06-25 15:23:51 +0200

    formulaireFormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { NgClass } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import{MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'crm-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,  LoginComponent,MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
