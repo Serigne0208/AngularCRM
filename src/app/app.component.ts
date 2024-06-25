8f4af480b2635b022755401ace21113aab362a54
serigne-saliou sow <saliousow0208@gmail.com>
2024-06-25 16:13:28 +0200

    component dummy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import{MatToolbarModule} from '@angular/material/toolbar';
+import { DummyComponent } from './component/dummy/dummy.component';
 @Component({
   selector: 'crm-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,  LoginComponent,MatToolbarModule],
+  imports: [CommonModule, RouterOutlet,  LoginComponent,MatToolbarModule, DummyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angularCRM';
+
+  received($event:string):void{
+    console.log($event)
+  }
 }
---
+++ src/app/component/dummy/dummy.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'crm-dummy',
+  standalone: true,
+  imports: [],
+  templateUrl: './dummy.component.html',
+  styleUrl: './dummy.component.scss'
+})
+export class DummyComponent {
+    @Input()
+    aValue?:string;
+
+    @Output()
+    sended: EventEmitter<string> = new EventEmitter<string>();
+
+    clicked() :void{
+      this.sended.emit(`you gave me ${this.aValue}`)
+    }
+}
